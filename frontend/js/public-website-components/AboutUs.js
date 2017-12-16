2490b6c3c69a4222947764f4a862fb3e6fb71a84
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-12-16 18:37:34 +0300

    Add location segment to about-us page

--- frontend/js/public-website-components/AboutUs.js
+++ frontend/js/public-website-components/AboutUs.js
@@ -8,7 +8,8 @@ import {
     Icon,
     Segment,
     Grid,
-    Container
+    Container,
+    Embed
 } from 'semantic-ui-react';
 
 type PropsType = {
@@ -21,14 +22,16 @@ export default class AboutUs extends Component<PropsType, StateType> {
     _yusuf = require('../../assets/images/yusuf.png');
     _hasan = require('../../assets/images/hasan.png');
     _fituri = require('../../assets/images/fituri.jpg');
+    // _qstp = require('../../assets/images/qstp.jpg');
+
     render() {
         return (
             <div>
                 <Segment style={ styles.teamSegment }>
                     <Container>
-                        <Header as='h3' style={{ fontSize: '2em' }}>Team</Header>
-                        <br/>
                         <Grid textAlign='center' columns={3} stackable>
+                            <Header as='h3' style={{ fontSize: '2em' }}>Team</Header>
+                            <br/>
                             <Grid.Row>
                                 <Grid.Column>
                                     <Card style={ styles.hasanCard }>
@@ -105,13 +108,28 @@ export default class AboutUs extends Component<PropsType, StateType> {
 
                 <Segment inverted style={ styles.locationSegment }>
                     <Container>
-                        <Grid textAlign='center' stackable>
+                        <Grid textAlign='center' columns={2} stackable>
                             <Grid.Row>
-                                <Header as='h3' style={{ fontSize: '2em' }}>Location</Header>
+                                <Header inverted as='h3' style={{ fontSize: '2em' }}>Location</Header>
                                 <br/>
                             </Grid.Row>
                             <Grid.Row>
-
+                                <Grid.Column>
+                                    <div style={ styles.qstpImage }></div>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Embed
+                                        style={ styles.embededMap }
+                                        active={true}
+                                        url='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3606.3732234493864!2d51.435260451148345!3d25.325254183757377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45dc1b48888243%3A0xc99991c5589f3b25!2sQatar+Science+and+Technology+Park!5e0!3m2!1sen!2sus!4v1486423672733'
+                                    />
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Header inverted as='h5' style={{ fontSize: '1em' }}>
+                                    Qatar Science & Technology Park is a home for international technology companies in Qatar,
+                                    and an incubator of start-up technology businesses.
+                                </Header>
                             </Grid.Row>
                         </Grid>
                     </Container>
@@ -144,5 +162,16 @@ const styles = {
         padding: '8em 0em',
         borderRadius: 0,
         margin: 0
+    },
+    qstpImage: {
+        backgroundImage: 'url(\'' + require('../../assets/images/qstp.jpg') + '\')',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'calc(50% + 13px) 50%',
+        height: '100%'
+    },
+    embededMap : {
+        borderRadius: '.3125em'
     }
+
 }
