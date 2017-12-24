b2a062da6475651091c2fa344354635d756f3e3d
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-12-24 16:59:24 +0300

    Change Contact to Contact Us Add error message alert Add Mail icon

--- frontend/js/public-website-components/Content.js
+++ frontend/js/public-website-components/Content.js
@@ -5,14 +5,14 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Features from './Features';
 import AboutUs from './AboutUs';
-import Contact from './Contact'
+import ContactUs from './ContactUs'
 
 
 const Content = () => (
     <Switch>
       <Route path='/features' component={Features}/>
       <Route path='/about-us' component={AboutUs}/>
-      <Route path='/contact' component={Contact}/>
+      <Route path='/contact-us' component={ContactUs}/>
       <Route exact path='/' component={Home}/>
     </Switch>
 )
