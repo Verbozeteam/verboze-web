970277b934a2f1cc4a24d1134c3db806bcb514b2
Yusuf Musleh <yusuf.musleh@gmail.com>
2018-01-27 12:37:53 +0300

    Update banner call to action message

--- frontend/js/public-website-components/RequestDemoBanner.js
+++ frontend/js/public-website-components/RequestDemoBanner.js
@@ -33,7 +33,7 @@ class RequestDemoBanner extends React.Component<PropsType, StateType> {
       <div className='container-fluid'>
         <div className='row justify-content-around' style={styles.container}>
           <div className='col-lg-6 col-md-8 col-12'>
-            <h2 style={styles.header}>See Verboze in action in your Hotel.</h2>
+            <h2 style={styles.header}>See Verboze in action at your Hotel.</h2>
           </div>
           <div className='col-lg-4 col-md-4 col-12'>
             {this._renderButton()}
