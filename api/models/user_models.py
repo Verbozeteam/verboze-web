12c4ba4309ddc472492c9fbeb83aabd96ca642f3
Yusuf Musleh <yusuf.musleh@gmail.com>
2018-10-09 16:15:26 +0300

    Add more clear __str__ for User model Add AdminUser model to represent Django Admin Create migrations to reflect changes Create data migration to create AdminUsers for existing admins

--- api/models/user_models.py
+++ api/models/user_models.py
@@ -7,14 +7,32 @@ from django.contrib.auth.models import AbstractUser
 # It would be much more difficult to do so mid-project after initial migrations.
 class User(AbstractUser):
     def __str__(self):
+        user_types = []
+        if hasattr(self, 'admin_user'):
+            user_types.append('admin_user')
         if hasattr(self, 'guest_user'):
-            return "[GUEST USER] {}".format(self.username)
-        elif hasattr(self, 'hotel_user'):
-            return "[HOTEL USER] {}".format(self.username)
-        elif hasattr(self, 'hub_user'):
-            return "[HUB USER] {}".format(self.username)
-        else:
+            user_types.append('GUEST USER')
+        if hasattr(self, 'hotel_user'):
+            user_types.append('HOTEL USER')
+        if hasattr(self, 'hub_user'):
+            user_types.append('HUB USER')
+
+        if len(user_types) == 0:
             return self.username
+        else:
+            return self.username + ' -> ' + str(user_types)
+
+class AdminUser(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='admin_user'
+    )
+
+    def __str__(self):
+        return '[ADMIN USER] {}'.format(
+            self.user.username
+        )
 
 class GuestUser(models.Model):
     user = models.OneToOneField(
@@ -24,9 +42,8 @@ class GuestUser(models.Model):
     )
 
     def __str__(self):
-        return '[GUEST USER] {} {}'.format(
-            self.user.first_name,
-            self.user.last_name
+        return '[GUEST USER] {}'.format(
+            self.user.username
         )
 
 
@@ -45,14 +62,13 @@ class HotelUser(models.Model):
     )
 
     def __str__(self):
-        return '[HOTEL USER] {} {}'.format(
-            self.user.first_name,
-            self.user.last_name
+        return '[HOTEL USER] {}'.format(
+            self.user.username,
         )
 
     @property
     def websocket_group(self):
-        return "hotel-{}".format(self.hotel.id)
+        return 'hotel-{}'.format(self.hotel.id)
 
     def ws_send_message(self, message):
         # send message to this hotel's group (only contains this hotel)
@@ -74,14 +90,13 @@ class HubUser(models.Model):
     )
 
     def __str__(self):
-        return '[HUB USER] {} {}'.format(
-            self.user.first_name,
-            self.user.last_name
+        return '[HUB USER] {}'.format(
+            self.user.username
         )
 
     @property
     def websocket_group(self):
-        return "hub-{}".format(self.hub.id)
+        return 'hub-{}'.format(self.hub.id)
 
     def ws_send_message(self, message):
         # send message to this hub's group (only contains this hub)
